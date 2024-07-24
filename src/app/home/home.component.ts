decbc39519e3e982e1893b76a001fc2aefc7dc95
Cesare Malara <gerundo@live.it>
2024-07-24 10:53:43 +0200

    a

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,29 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {MatGridListModule} from '@angular/material/grid-list';
 import { LeaderboardMenuComponent } from '../leaderboard-menu/leaderboard-menu.component';
 import { MapComponent } from '../map/map.component';
 import { TroopMenuComponent } from '../troop-menu/troop-menu.component';
 
+export interface Tile {
+  color: string;
+  cols: number;
+  rows: number;
+  text: string;
+}
+
+/**
+ * @title Dynamic grid-list
+ */
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [LeaderboardMenuComponent, MapComponent, TroopMenuComponent],
+  imports: [MatGridListModule,LeaderboardMenuComponent, MapComponent, TroopMenuComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent 
-{
+export class HomeComponent {
+  tiles: Tile[] = [
+    {text: 'One', cols: 7, rows: 2, color: 'lightblue'},
+    {text: 'Two', cols: 3, rows: 1, color: 'lightgreen'},
+  ];
 }
